ab3d9f4770bce01b821622a3af125889cb5eb5f1
Gravityboots <117709053+gravityboots@users.noreply.github.com>
2026-08-15 21:05:48 +0530

    update: fastfetch display script

--- .local/bin/rice_flex.sh
+++ .local/bin/rice_flex.sh
@@ -4,15 +4,14 @@
 # hyprctl dispatch movefocus r
 
 hyprctl dispatch movecursor 1920 1080
-
+sleep 0.2
 hyprctl dispatch exec "kitty --hold -e btop"
-
 hyprctl dispatch exec "kitty --hold -e fastfetch"
-
+sleep 0.2
+hyprctl dispatch resizewindowpixel 100 0, activewindow
 hyprctl dispatch exec "kitty --hold -e cmatrix"
 sleep 0.2
 hyprctl dispatch resizewindowpixel 0 40, activewindow
-
 hyprctl dispatch exec "kitty --hold"
 sleep 0.2
 hyprctl dispatch resizewindowpixel 40 0, activewindow
