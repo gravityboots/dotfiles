0f8e1d4b0a5a27c7606432466db4aafed05f8670
Gravityboots <117709053+gravityboots@users.noreply.github.com>
2026-08-30 20:56:46 +0530

    update: vscodium, noctalia, hyrpland configs; rice_flex script fix; termius launch config

--- .local/bin/rice_flex.sh
+++ .local/bin/rice_flex.sh
@@ -4,14 +4,14 @@
 # hyprctl dispatch movefocus r
 
 hyprctl dispatch movecursor 1920 1080
-sleep 0.2
+sleep 0.25
 hyprctl dispatch exec "kitty --hold -e btop"
 hyprctl dispatch exec "kitty --hold -e zsh -ic fastfetch"
-sleep 0.2
+sleep 0.25
 hyprctl dispatch resizewindowpixel 100 0, activewindow
 hyprctl dispatch exec "kitty --hold -e cmatrix"
-sleep 0.2
+sleep 0.25
 hyprctl dispatch resizewindowpixel 0 40, activewindow
 hyprctl dispatch exec "kitty --hold"
-sleep 0.2
+sleep 0.25
 hyprctl dispatch resizewindowpixel 40 0, activewindow
